0ff10aecbdc71c381f3ba103548648acf5c67404
Gabriel codinhoto <codinhoto18@gmail.com>
2024-04-05 23:22:23 -0300

    Criação da navegação descktop, Remoção da classe header-mobile e inicio da responsividade

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 const body = document.querySelector("body")
-const nav = document.querySelector("header-mobile nav")
+const nav = document.querySelector("header .mobile")
 
-botaoAbrir.addEventListener('click', abrirmenu)
-botaoFechar.addEventListener('clilk, fecharmenu')
+botaoAbrir.addEventListener('click', abrirMenu)
+botaoFechar.addEventListener('clilk', fecharMenu)
 
-function abrirmenu() {
+function abrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
 }
 
-function fecharmenu() {
+function fecharMenu() {
     body.classList.remove("escurecer")
     nav.classList.remove("abrir")
 }
